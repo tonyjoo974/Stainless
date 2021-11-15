e9b30d8b7e0f942b94928b0c1a72fbb7cc1f1ef3
Bora Shim <boras2@illinois.edu>
2021-11-14 21:51:16 -0600

    stain_activity layout

--- app/src/main/java/edu/illinois/cs465/stainless/Stain_Activity.java
+++ app/src/main/java/edu/illinois/cs465/stainless/Stain_Activity.java
@@ -1,31 +1,71 @@
 package edu.illinois.cs465.stainless;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class Stain_Activity extends AppCompatActivity {
 
     private TextView title;
     private ImageView img;
+    ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stain);
 
-//        title = (TextView) findViewById(R.id.txtName);
-//        img = (ImageView) findViewById(R.id.stainThumbnail);
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Add to Favorite
+        toggleButton = (ToggleButton) findViewById(R.id.myToggleButton);
+        toggleButton.setChecked(false);
+        toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.img_star_grey));
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.img_star_yellow));
+                else
+                    toggleButton.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.img_star_grey));
+            }
+        });
+
+        title = (TextView) findViewById(R.id.txtName);
+        img = (ImageView) findViewById(R.id.stainThumbnail);
 
         // Receive data
         Intent intent = getIntent();
-        String stainName = intent.getExtras().getString("StainName");
-        int image = intent.getExtras().getInt("Thumbnail");
+        String stainName = intent.getExtras().getString("name");
+        int image = intent.getExtras().getInt("thumbnail");
 
-//        title.setText(stainName);
-//        img.setImageResource(image);
+        title.setText(stainName);
+        img.setImageResource(image);
+
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
